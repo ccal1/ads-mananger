e075eefdb2a6a2a69e797a823512ff82840199f0
caio.lima <caio.lima@inlocomedia.com>
2018-09-29 19:10:26 -0300

    fix(build.gradle)

---
+++ app/src/main/java/com/mananger/ads/CampaignsApi.java
@@ -0,0 +1,13 @@
+package com.mananger.ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignsApi {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CampaignsApi.class, args);
+  }
+}
+
